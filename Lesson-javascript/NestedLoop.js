638337b522291c595e9bcdf601c7b926ae2b8829
rifkyhilman <rifkyhilman265@gmail.com>
2024-03-13 11:15:38 +0700

    :memo: Add nested loop with array 2d
    add nested loop with array 2d

--- Lesson-javascript/NestedLoop.js
+++ Lesson-javascript/NestedLoop.js
@@ -1,8 +1,22 @@
-const pilihan = "abcde";
+// const pilihan = "abcde";
 
-for (let i = 1; i <= 10; i++){
-    console.log(`${i}. Soal nomor ${i}`);
-    for(let j = 0; j < pilihan.length; j++){
-        console.log(` ${pilihan[j]}. Pilih Jawaban `);
+// for (let i = 1; i <= 10; i++){
+//     console.log(`${i}. Soal nomor ${i}`);
+//     for(let j = 0; j < pilihan.length; j++){
+//         console.log(` ${pilihan[j]}. Pilih Jawaban `);
+//     };
+// };
+
+const studentRow = [
+    ['Oliver', 'Liam', 'Ujang', 'Noah'],
+    ['Amelia', 'Steven', 'Ivan', 'Arip'],
+    ['Sophia', 'Mateo', 'Isabella', 'Lucas']
+]
+
+for(let i = 0; i < studentRow.length; i++){
+    const row = studentRow[i];
+    console.log(`Seat Row # ${i + 1 }`);
+    for(let j = 0; j < row.length; j++){
+        console.log(`  ${row[j]}`);
     };
 };
